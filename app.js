a1b2e279b862bc752284d7279b5f5bfca2ddcb23
Ryan Newell <ryanalbertnewell@gmail.com>
2018-06-11 14:25:25 -0500

    Lots of packages.

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
+});
